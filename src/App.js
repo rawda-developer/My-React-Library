f4bcf229f6197f918933367ec64f4c67d3a19c8e
Rawda Yasser <rawda.developer@gmail.com>
2022-06-05 02:17:30 +0200

    Feat: add getLocalStorage component and render it

--- src/App.js
+++ src/App.js
@@ -7,10 +7,21 @@ function App() {
     const data = await axios.get(url);
     return data.data;
   };
+
+  const getLocalStorage = (key) => () => {
+    const data = localStorage.getItem(key);
+    return data;
+  };
+  const Text = ({ message }) => {
+    return <div>{message}</div>;
+  };
   return (
     <>
       <DataLoader resourceName='user' getData={getData('/current-user')}>
         <UserInfo />
+      </DataLoader>
+      <DataLoader resourceName='message' getData={getLocalStorage('message')}>
+        <Text />
       </DataLoader>
     </>
   );
