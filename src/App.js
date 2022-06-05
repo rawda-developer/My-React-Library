44af2fe3004911a435fa19b07097a2e5bed28a5b
Rawda Yasser <rawda.developer@gmail.com>
2022-06-05 03:21:14 +0200

    Feat: add modal component as a Controlled component and render it on App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import { ControlledForm } from './contolledAndUncontrolled/ControlledForm';
-import { UncontrolledForm } from './contolledAndUncontrolled/UncontrolledForm';
+import { useState } from 'react';
+import { ControlledModal } from './contolledAndUncontrolled/ControlledModal';
 
 function App() {
+  const [shouldShow, setShouldShow] = useState(false);
   return (
     <>
-      <ControlledForm />
+      <button onClick={() => setShouldShow(!shouldShow)}>
+        {shouldShow ? 'Hide Form' : 'Show Form'}
+      </button>
+      {shouldShow && (
+        <ControlledModal
+          shouldShow={shouldShow}
+          shouldShowControl={() => setShouldShow(!shouldShow)}
+        >
+          <h2>Hello world</h2>
+        </ControlledModal>
+      )}
     </>
   );
 }
---
+++ src/contolledAndUncontrolled/ControlledModal.js
@@ -0,0 +1,54 @@
+import styled from 'styled-components';
+import React from 'react';
+
+const ModalContainer = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 100;
+  overflow: auto;
+  padding: 20px;
+  box-sizing: border-box;
+  outline: none;
+  border: none;
+  border-radius: 5px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+`;
+const ModalBody = styled.div`
+  padding: 10px;
+  box-sizing: border-box;
+  font-size: 1.2em;
+  font-family: 'Roboto', sans-serif;
+  font-weight: 300;
+  line-height: 1.5;
+  text-align: center;
+  text-decoration: none;
+  background-color: #fff;
+  border-radius: 5px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+  width: 50%;
+  margin: 10% auto;
+  padding: 20px;
+`;
+
+export const ControlledModal = ({
+  children,
+  shouldShow,
+  shouldShowControl,
+}) => {
+  return (
+    <>
+      {shouldShow && (
+        <ModalContainer onClick={shouldShowControl}>
+          <ModalBody>{children}</ModalBody>
+        </ModalContainer>
+      )}
+    </>
+  );
+};
